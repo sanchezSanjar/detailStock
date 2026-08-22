2881c18e50b2aaf978eb6452bf6e645d0cbd6d24
SanjarSanchez <sanjar@ajou.ac.kr>
2026-08-23 01:28:49 +0900

    fix:modify UpdateChosenProduct business logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -46,9 +46,14 @@ productController.createNewProduct = async (
 productController.updateChosenProduct = async (req: Request, res: Response) => {
     try {
         console.log("getAllProducts");
+        const id = req.params.id as string;
         
+        const result = await productService.updateChosenProduct( id, req.body);
+
+        res.status(HttpCode.OK).json({data: result});
     } catch (err) {
         console.log("Error, updateChosenProduct:", err);
+        
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard)};
     };
