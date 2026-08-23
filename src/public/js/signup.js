91fddce788a913a13ecb1e9bb3c93083ae20ba53
SanjarSanchez <sanjar@ajou.ac.kr>
2026-08-23 17:15:21 +0900

    fix: admin project refactoring

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -6,10 +6,10 @@ $(function () {
 
   fileTarget.on("change", function () {
     if (window.FileReader) {
-      const uploadFile = $(this)[0].files[0];
-      //console.log("uploadFile:", uploadFile);
-      const fileType = uploadFile["type"];
-      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      const uploadFile = $(this)[0].files[0],
+        //console.log("uploadFile:", uploadFile);
+        fileType = uploadFile["type"],
+        validImageType = ["image/jpg", "image/jpeg", "image/png"];
       if (!validImageType.includes(fileType)) {
         alert("Please insert only jpeg, jpg and png!");
       } else {
@@ -28,11 +28,11 @@ $(function () {
 
 function validateSignupForm() {
   //console.log("EXECUTED validateSignupForm")
-  const memberNick = $(".member-nick").val();
-  //console.log("memberNick:", memberNick)
-  const memberPhone = $(".member-phone").val();
-  const memberPassword = $(".member-password").val();
-  const confirmPassword = $(".confirm-password").val();
+  const memberNick = $(".member-nick").val(),
+    //console.log("memberNick:", memberNick)
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
 
   if (
     memberNick === "" ||
