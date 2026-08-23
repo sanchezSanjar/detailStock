c53d891b628f418f3454d89550a7478dcb7ae02c
SanjarSanchez <sanjar@ajou.ac.kr>
2026-08-23 23:15:51 +0900

    feat: CORS integration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import path from "path";
 import router from "./router";
@@ -20,9 +21,15 @@ const store = new MongoDBStore({
 // Entrance
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static("./uploads"))
+app.use("/uploads", express.static("./uploads"));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  }),
+);
 app.use(cookieParser());
 app.use(
   morgan(MORGAN_FORMAT, {
